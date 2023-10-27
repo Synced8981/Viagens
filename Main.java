0131efd04549dfddde4ad9df403859c2cebb4e89
Synced8981 <edufodatutoriaisbr@gmail.com>
2023-10-27 09:56:41 -0300

    Mexi um pouco no main e no turista

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args){
+        Turista mochileiro = new Turista();
+        mochileiro.nome = "Adriano";
+
+        Turista novoTurista = new Turista("Maria");
+        novoTurista.nome = "Maria";
+
+
+
+
+    }
+}
---
+++ Turista.java
@@ -0,0 +1,17 @@
+public class Turista {
+    // Atributo
+
+    public String nome;
+    public String cpf;
+
+    //construtor padrão
+
+    Turista(){
+
+    }
+
+    //Sobrecarga do construtor
+     Turista(String nome){
+        this.nome = nome;
+     }
+}
